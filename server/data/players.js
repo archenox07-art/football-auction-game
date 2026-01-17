d808c8ba5c00179df96888ee1c33761e9dd3b323
ARCHISHMAN DAS <archenox07@gmail.com>
2026-01-18 03:22:15 +0530

    Create players.js

---
+++ server/data/players.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, name: "Lionel Messi", position: "ATT" },
+  { id: 2, name: "Cristiano Ronaldo", position: "ATT" },
+  { id: 3, name: "Kevin De Bruyne", position: "MID" },
+  { id: 4, name: "Virgil van Dijk", position: "DEF" },
+  { id: 5, name: "Alisson Becker", position: "GK" }
+];
