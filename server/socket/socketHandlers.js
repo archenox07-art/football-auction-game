88a2315d7ba407a810dd49d28a9f39d9ba5392c6
ARCHISHMAN DAS <archenox07@gmail.com>
2026-01-18 03:20:28 +0530

    Create socketHandlers.js

---
+++ server/socket/socketHandlers.js
@@ -0,0 +1,36 @@
+import {
+  createRoom,
+  joinRoom,
+  removePlayer
+} from "../rooms/roomManager.js";
+
+import {
+  startAuction,
+  placeBid
+} from "../auction/auctionManager.js";
+
+export default function registerSocketHandlers(io) {
+  io.on("connection", (socket) => {
+
+    socket.on("create_room", (data) => {
+      createRoom(io, socket, data);
+    });
+
+    socket.on("join_room", (data) => {
+      joinRoom(io, socket, data);
+    });
+
+    socket.on("start_auction", (data) => {
+      startAuction(io, socket, data);
+    });
+
+    socket.on("place_bid", (data) => {
+      placeBid(io, socket, data);
+    });
+
+    socket.on("disconnect", () => {
+      removePlayer(io, socket);
+    });
+
+  });
+}
